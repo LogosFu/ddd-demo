40371a3871d5a3dafb62917bad2ba90d5141ea32
shiwei.fu <shiwei.fu@thoughtworks.com>
2019-12-08 14:22:13 +0800

    #

---
+++ src/main/java/com/ddd/demo/InitData.java
@@ -0,0 +1,11 @@
+package com.ddd.demo;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class InitData implements CommandLineRunner {
+
+  @Override
+  public void run(String... args) throws Exception {
+
+  }
+}
