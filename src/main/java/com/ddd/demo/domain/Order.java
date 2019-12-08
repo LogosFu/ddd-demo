e8181e01a0ecd00880d9b91acb9692992357ae33
shiwei.fu <shiwei.fu@thoughtworks.com>
2019-12-08 11:45:34 +0800

    #

--- src/main/java/com/ddd/demo/domain/Order.java
+++ src/main/java/com/ddd/demo/domain/Order.java
@@ -1,5 +1,12 @@
 package com.ddd.demo.domain;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public class Order {
 
+  private Integer id;
+  private Integer userId;
+  private BigDecimal freight;
+  private List<OrderItem> orderItems;
 }
--- src/main/java/com/ddd/demo/domain/OrderItem.java
+++ src/main/java/com/ddd/demo/domain/OrderItem.java
@@ -1,5 +1,14 @@
 package com.ddd.demo.domain;
 
+import java.math.BigDecimal;
+
 public class OrderItem {
 
+  private BigDecimal price;
+  private Integer count;
+  private Integer goodsId;
+
+  public BigDecimal getAmount() {
+    return null;
+  }
 }
